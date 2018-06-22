3904cad44d9c7a29416644df6e39c1b1f7f19037
James Massardo <jmassardo@chef.io>
2018-06-21 22:46:43 -0500

    adding additional tasks to address CIS package-08

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,3 +5,12 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
 package 'auditd'
+
+template '/etc/audit/auditd.conf' do
+  source 'audit.conf.erb'
+  notifies :restart, 'service[auditd]', :immediately
+end
+
+service 'auditd' do
+  action :nothing
+end
