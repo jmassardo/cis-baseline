b1ab53428024f53dcce41193c940336b06fd7669
James Massardo <jmassardo@chef.io>
2018-12-04 10:26:09 -0600

    change case to use proper platform object
    Signed-off-by: James Massardo <jmassardo@chef.io>

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,7 +4,7 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-case node['platform_family']
+case node['platform']
 when 'windows'
   include_recipe 'cis_baseline::windows'
 when 'ubuntu'
