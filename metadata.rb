f4455e8465f6e30671a3246410e2cc4573da3c3a
James Massardo <james@dxrf.com>
2020-05-12 14:28:01 +0000

    reset sysctl

--- metadata.rb
+++ metadata.rb
@@ -3,18 +3,7 @@ maintainer 'James Massardo'
 maintainer_email 'james@dxrf.com'
 license 'All Rights Reserved'
 description 'Installs/Configures cis-baseline'
-long_description 'Installs/Configures cis-baseline'
-version '0.2.12'
-chef_version '>= 12.14' if respond_to?(:chef_version)
+version '0.2.13'
 supports 'ubuntu'
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
 issues_url 'https://github.com/jmassardo/cis-baseline/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
 source_url 'https://github.com/jmassardo/cis-baseline'
